70f0a3c2ad4183292971f359dbb944dec98decd3
DavidKielty1 <davidkielty@live.co.uk>
2021-12-23 14:59:35 +0000

    works slider complete

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import "./works.scss";
+
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const data = [
     {
       id: "1",
@@ -23,9 +27,18 @@ export default function Works() {
       img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
     },
   ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
   return (
     <div className="works" id="works">
-      <div className="slider">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         {data.map((d) => (
           <div className="container">
             <div className="item">
@@ -46,8 +59,22 @@ export default function Works() {
           </div>
         ))}
       </div>
-      <img src="assets/arrow.png" className="arrow left" alt="" />
-      <img src="assets/arrow.png" className="arrow right" alt="" />
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => {
+          handleClick("left");
+        }}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => {
+          handleClick("");
+        }}
+      />
     </div>
   );
 }

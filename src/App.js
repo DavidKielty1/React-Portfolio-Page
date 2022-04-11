f9fef386642215a571771f80402d5aadd71a3753
DavidKielty1 <davidkielty@live.co.uk>
2022-04-11 10:38:29 +0100

    Heroku update took away portfolio section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
-import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
@@ -17,7 +16,6 @@ function App() {
       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
-        <Portfolio />
         <Works />
         <Testimonials />
         <Contact />
